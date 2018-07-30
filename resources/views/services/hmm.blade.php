cf7aac106e13b1ebd7ec1f2cb06e07e6641e0d8e
radean <radeanf@gmail.com>
2018-07-30 18:41:36 +0500

    added Pictures

--- resources/views/services/hmm.blade.php
+++ resources/views/services/hmm.blade.php
@@ -14,47 +14,37 @@
         </div>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm half-height p-4 text-center baseColor">
-                    <h1 class="font-weight-bold big-headings pr-5 ">
+                <div class="col-sm quarter-height p-5 baseColor text-center">
+                    <h1 class="font-weight-bold big-headings-Style text-justify">
                         HMM is a global integrated
                         logistics company providing
                         the best transport services.
                     </h1>
                 </div>
-                <div class="col-sm">
-                    <h4 class="text-justify pl-5 p-5">
+                <div class="col-sm patternized">
+                    <h5 class="text-justify pl-5 p-5">
                         Our professionally skilled and experienced staffs
                         provide customers with world-class transportation
                         services via more than 100 state-of-art vessels and
                         various logistics facilities that span a worldwide
                         network of ports together with industry-leading
                         IT systems.
-                    </h4>
+                    </h5>
                 </div>
             </div>
         </div>
         <div class="container p-5">
-            <div class="row">
-                <div class="col-sm">
-                    <!--HMM Services-->
+            <!--Text-->
+            <!--HMM Vessels Services-->
+            <div class="row mt-5">
+                <div class="col-sm-4 text-center">
                     <h2>
-                        HMM is a global integrated
-                        logistics company providing
-                        the best transport services.
+                        <span class="font-weight-bold"> HMM </span>  provides many types of
+                        <span class="font-weight-bold"> shipping </span> services with various vessels.
                     </h2>
-                    <p class="text-justify">
-                        Our professionally skilled and experienced staffs
-                        provide customers with world-class transportation
-                        services via more than 100 state-of-art vessels and
-                        various logistics facilities that span a worldwide
-                        network of ports together with industry-leading
-                        IT systems.
-                    </p>
-                    <h2>
-                        HMM provides many types of
-                        shipping services with various vessels.
-                    </h2>
-                    <p class="text-justify">
+                </div>
+                <div class="col-sm-8">
+                    <h5 class="text-justify">
                         Since launching our first service with three VLCCs
                         (Very Large Crude Carriers) in 1976, HMM has
                         substantially expanded into the world-class integrated
@@ -65,14 +55,24 @@
                         has gained a diverse business portfolio and structure
                         that can yield stable revenue despite the market
                         change in certain fields of the shipping industry.
-                    </p>
+                    </h5> <br>
+                </div>
+            </div>
+            <!--Pictured-Text-->
+            <!--HMM World top Carriers-->
+            <div class="row mt-5">
+                <div class="col-sm-8">
+                    <img src="{{ url('/') }}/img/bg/hmmCarrier.jpg" width="100%" height="100%" class="d-inline-block" alt="">
+                </div>
+                <div class="col-sm-4 text-center">
                     <h2>
-                        HMM is one of the world’s top ocean carriers
+                        <span class="font-weight-bold"> HMM </span> is one of the world’s top <span class="font-weight-bold"> ocean </span> carriers
                     </h2>
-                    <p class="text-justify">
+                    <hr class="m-5">
+                    <h5 class="text-justify">
                         HMM has a vast global network of 4 overseas chief
                         offices, supported by 3 regional headquarters,
-                        23 subsidiaries, supported by three head offices,
+                        23 subsidiaries, supported by three head offices,zuk
                         58 branch offices, 8 overseas offices, and 3 container
                         terminals around the world. It has the reputation as
                         a world-class integrated logistics company due to its
@@ -83,37 +83,55 @@
                         network. Backed by these competencies, HMM is
                         leading the global shipping markets, where cross-
                         border competition intensifies each day.
-                    </p>
+                    </h5> <br>
+                </div>
+            </div>
+            <!--Pictured-Text-->
+            <!--HMM World top Carriers-->
+            <div class="row mt-5">
+                <div class="col-sm-4 text-center">
                     <h2>
-                        HMM serves as a main artery of the national economy.
+                        <span class="font-weight-bold"> HMM </span> serves as a main artery of the <span class="font-weight-bold"> national economy </span> .
                     </h2>
-                    <p class="text-justify">
+                    <hr class="m-5">
+                    <h5 class="text-justify">
                         HMM transports strategic national commodities
                         such as crude oil, coal/iron ore, and various special
                         cargoes as well as import/export goods. In doing so,
                         HMM plays a pivotal role in Korea’s economy,
                         generating more than USD 4 billion annually.
-                    </p>
+                    </h5> <br>
+                </div>
+                <div class="col-sm-8">
+                    <img src="{{ url('/') }}/img/bg/hmmEconomy.jpg" width="100%" height="100%" class="d-inline-block" alt="">
+                </div>
+            </div>
+            <!--Pictured-Text-->
+            <!--HMM World top Carriers-->
+            <div class="row mt-5 hmmGlobalBG text-white p-5">
+                <div class="col-sm-4 text-center">
+                    <img src="{{ url('/') }}/img/logos/hmm.png" alt="">
+                </div>
+                <div class="col-sm-8">
                     <h2>
-                        Global service network with more than 60 sea routes and over 100 ports of call
-                        enables HMM to collaborate more strategically with the world’s major shipping companies.
-                    </h2>
-                    <p class="text-justify">
-                        Since its inception in 1976, Hyundai Merchant Marine (HMM) has continuously endeavored to become a premier global shipping and logistics company.
-                        HMM is currently one of the top five Trans-Pacific carriers and one of the top 10 carriers in the global shipping industry with a varied sized fleet for Trans-Pacific, Asia-Europe, Trans-Atlantic and Inter-Asia trade.
-                        Our legacy of customer service excellence proudly contin-
-                        ues. For example, HMM has expanded container service to niche markets in South America, Australia and Russia.
-                        Other significant accomplishments include: expansion of inland live reefer service, improved Regional Customer Service Center (RCSC) service, service, and upgraded electronic service on the Mobile devices - sailing schedules, rate requests, online booking, cargo tracking & tracing, and Internet B/L service, etc.
-
-                        Future plans include the upsizing of our vessels, capacity increase in special container service (Jumbo and Reefer service), extension of service scope to new markets, and linking the finest internet shipping portal in the world providing complete and fully integrated customer services on the Web. In addition, HMM will as always, continue to add new shipping routes and tonnage, resources to improve electronic services, and further streamline existing programs, services, etc.
-                        Ever striving to be the best, we plan to launch strategic initiatives to ensure even greater capacity, flexibility and quality for the new millennium. As with all HMM plans, these strategies are acutely focused on increasing customer service and satisfaction.
-
-                        With the ending of the G6 consortium in April HMM formed in April 2017, a co-operation agreement with the 2M Alliance providing a competitive network covering services between Asia and West Coast USA, Asia and East Coast USA, Asia and North Europe, Asia and the Mediterranean, Europe/North America and North America/Europe trades.
-                        In addition, HMM established a new extensive Intra-Asia network(including Korea – Vietnam/Japan) with the cooperation of two other Korean Carriers.
-                        After all, our philosophy is simple: HMM is proactive in meeting customer demands and dedicated to customer satisfaction with a management team who, being customer oriented and committed to high business ethics, are constantly pursuing innovation and service expansion for customer benefit and satisfaction.
-                        Our continued success ensured, we can transfer happiness to individual, family and global community getting over the customer satisfaction through logistics solution.
-                    </p>
-
+                        <span class="font-weight-bold"> Global service network </span> with more than <span class="font-weight-bold"> 60 </span> sea routes and over 100 ports of call
+                        enables <span class="font-weight-bold"> HMM </span> to collaborate more strategically with the world’s major shipping companies.
+                    </h2> <br>
+                    <h5 class="text-justify">
+                        HMM has a vast global network of 4 overseas chief
+                        offices, supported by 3 regional headquarters,
+                        23 subsidiaries, supported by three head offices,zuk
+                        58 branch offices, 8 overseas offices, and 3 container
+                        terminals around the world. It has the reputation as
+                        a world-class integrated logistics company due to its
+                        vast global network, the highly skilled vessel
+                        operations and accurate market forecast by most
+                        qualified professional employees with vast shipping
+                        experiences together with advanced global IT system
+                        network. Backed by these competencies, HMM is
+                        leading the global shipping markets, where cross-
+                        border competition intensifies each day.
+                    </h5> <br>
                 </div>
             </div>
         </div>
