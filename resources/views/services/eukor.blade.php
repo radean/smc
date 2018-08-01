5c6b43728f5116bf3f58590398c0483f926a9970
radean <radeanf@gmail.com>
2018-08-01 19:13:20 +0500

    Added Eukor

--- resources/views/services/eukor.blade.php
+++ resources/views/services/eukor.blade.php
@@ -5,7 +5,7 @@
     <!--HMS-->
     <div class="container-fluid" style="padding: 0px">
         <!--Page BGS Image-->
-        <div class="row">
+        <div class="row">g
             <div class="col-sm-12 bgs hmmBG half-height p-5">
                 <div class="mx-auto p-5 mt-5 text-dark" style="width: fit-content;">
                     <h1 style="font-size: 3.25rem;" class="font-weight-bold text-white">EUKOR Car Carriers Inc.</h1>
